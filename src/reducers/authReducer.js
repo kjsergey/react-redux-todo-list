d24e7170ef024a247b1c9b372bc1d13bb8564300
Loker <therealloker@gmail.com>
2019-07-23 00:01:32 +0300

    add redux store and reducers

---
+++ src/reducers/authReducer.js
@@ -0,0 +1,16 @@
+import * as actions from '../actions/actionTypes'
+
+function authReducer(state = [], action) {
+  switch(action.type) {
+      case actions.AUTHENTICATED:
+        return { ...state, authenticated: true };
+      
+      case actions.UnAUTHENTICATED:
+        return { state: [], authenticated: false, error: action.error };
+
+      default:
+        return state;    
+  }
+}
+
+export default authReducer
