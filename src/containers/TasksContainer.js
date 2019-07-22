f32d66e96ca5e7a207609230f98004421b68432f
Loker <therealloker@gmail.com>
2019-07-22 22:27:59 +0300

    get tasks

---
+++ src/containers/TasksContainer.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react'
+import API from '../utils/API';
+
+class TasksContainer extends Component {
+  state = {
+    tasks: []
+  }
+  
+  getTasks() {
+    API.get('/tasks')
+    .then(response => {
+      this.setState({tasks: response.data})
+    })
+    .catch(error => console.log(error))
+  }
+
+  componentDidMount() {
+    this.getTasks()
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="inputContainer">
+          <input className="taskInput" type="text" placeholder="Add a task" onKeyPress={this.createTodo} />
+        </div>  	    
+        <div className="listWrapper">
+	        <ul className="taskList">
+		        {this.state.tasks.map((task) => {
+		          return(
+		            <li className="task" key={task.id}>
+			            <input className="taskCheckbox" type="checkbox" />              
+			            <label className="taskLabel">{task.name}</label>
+		            </li>
+		          )       
+		        })} 	    
+	        </ul>
+	      </div>
+      </div>    
+    )
+  }
+}
+
+export default TasksContainer
