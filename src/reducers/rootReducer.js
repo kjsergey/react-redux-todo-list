df3d7a4d4288823965c16e3653e128cb100f7340
Loker <therealloker@gmail.com>
2019-07-28 21:13:13 +0300

    add actions and reducer for tasks manage

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux'
 import authReducer from './authReducer'
+import todosReducer from './todosReducer';
 
 const rootReducer = combineReducers({
+  todos: todosReducer,
   auth: authReducer
 });
 
